342506e214109ce0031ab1f19bf41ef539016b4a
vladyslovestepanov <vstepanov@neonomics.io>
2022-04-30 15:59:56 +0300

    added Messages contract, tested in truffle console

--- contracts/Messenger.sol
+++ contracts/Messenger.sol
@@ -6,5 +6,25 @@ pragma solidity >=0.4.22 <0.9.0;
  * The Messenger contract does this and that...
  */
 contract Messenger {
+    address owner;
+    string[] messages;
+
+    constructor () public {
+        owner = msg.sender;
+    }
+
+// Data location must be "memory" or "calldata" for parameter in function, but none was given.
+    function add(string memory newMessage) public { 
+        require(msg.sender == owner);
+        messages.push(newMessage);
+    }
+
+    function count() view public returns(uint) {
+        return messages.length;
+    }
+
+    function getMessages(uint index) view public returns (string memory) {
+        return messages[index];
+    }
 
 }
