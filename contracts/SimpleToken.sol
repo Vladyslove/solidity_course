9775b2a2d01fca425973b6bf407a54190271e115
vladyslovestepanov <vstepanov@neonomics.io>
2022-05-03 00:32:52 +0300

    added few functions from hw lecture 19

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -3,27 +3,38 @@ pragma solidity >=0.4.22 <0.9.0;
 
 /**
 * after lecture 18.Mappings in solidity
+Create a function that returns the balance of a certain account
+
+Create a function that returns the available tokens supply
+
+Create a payable function that receives ether and it will send tokens to the sender.
 */
 
 contract SimpleToken {
-    address owner;
+    address payable owner;
     mapping (address => uint256) public accounts;
-
-//    error public NotEnoughMoney;
 
     constructor(uint256 initialSupply) public {
         owner = msg.sender;
         accounts[owner] = initialSupply;
     }
 
-    function transfer(address to, uint256 value) public {
-//        if (accounts[msg.sender] >= value) {
-//            revert NotEnoughMoney();
-//        }
-
-//        require(accounts[msg.sender] >= value);
+    function transfer(address to, uint256 value) payable public {
+        require(accounts[msg.sender] >= value);
         require(accounts[to] + value >= accounts[to]);
         accounts[msg.sender] -= value;
         accounts[to] += value;
+    }
+
+    function balance(address _address) public view returns(uint256){
+        return accounts[_address];
+    }
+
+    function supply() public view returns(uint256){
+        return accounts[owner];
+    }
+
+    function receiveFunds() payable external {
+        // lost here on how to receive ether and send tokens!
     }
 }
