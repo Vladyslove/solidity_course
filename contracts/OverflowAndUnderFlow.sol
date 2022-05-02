3f5e5bf6ddb3aff696e1b9cf6a858d00d890df7a
vladyslovestepanov <vstepanov@neonomics.io>
2022-05-02 22:13:35 +0300

    minor cleaning

--- contracts/OverflowAndUnderFlow.sol
+++ contracts/OverflowAndUnderFlow.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.9.0;
 
-// creating the contract
-/**
- * The Messenger contract does this and that...
- */
+
 contract OverflowAndUnderFlow {
 
     function overflow() public pure returns(uint256) {
