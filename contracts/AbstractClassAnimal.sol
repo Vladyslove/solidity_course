e44e36e10d9468d11bec4f2898ca07a6b1c0329e
vladyslovestepanov <vstepanov@neonomics.io>
2022-05-08 00:31:16 +0300

    added abstract class

---
+++ contracts/AbstractClassAnimal.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+/**
+*
+*/
+
+contract AbstractClassAnimal {
+    string public breed;
+    uint public age;
+    uint public weight;
+
+    constructor() public {
+        age = 1;
+        weight = 1;
+    }
+
+    function sleep() pure public returns (string memory) {return "Zzzzz...";}
+
+    function eat() pure public returns (string memory) {return "Nom nom..";}
+
+    function talk() pure public returns (string memory);
+}
+
+contract Cat is AbstractClassAnimal {
+    constructor() public {
+        breed = "Persian";
+        age = 3;
+        weight = 5;
+    }
+    function talk() pure public returns (string memory) {return "miaow";}
+}
+
+contract Dog is AbstractClassAnimal {
+    constructor() public {
+        breed = "Labrador";
+        age = 5;
+        weight = 3;
+    }
+    function talk() pure public returns (string memory) {return "bark bark";}
+}
